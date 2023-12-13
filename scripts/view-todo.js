a073e63f379975b5caa04c59a885301d3beb8625
Juan11211 <avilesjuan628@gmail.com>
2023-12-12 22:09:51 -0500

    completed users dropdown, able to see todos in console.log

--- scripts/view-todo.js
+++ scripts/view-todo.js
@@ -1,17 +1,47 @@
 "use strict"
 
-window.onload = function(){
-
+window.onload = () => {
     fetchUserApi();
 
 }
 
 function fetchUserApi(){
-    // fetch
-
-    // dropdown 
+    fetch(`http://localhost:8083/api/users`)
+    .then(res => res.json())
+    .then(users => displayDropDown(users))
+    .catch(err => {
+        console.error(`THIS IS NOT FETCHING THE USERS API`, err)
+    })
 }
 
-function displayDropDown(){
-    
+function displayDropDown(users){
+    let userDropDown = document.getElementById('selectUserDropdown')
+
+    users.forEach(user => {
+       let options = new Option(user.name, user.id)
+       userDropDown.appendChild(options)
+    });
+
+    userDropDown.onchange = () => {
+        let selectedUserId = userDropDown.value;
+        fetchUserTodos(selectedUserId);
+    };
+}
+
+function fetchUserTodos(userId) {
+    fetch(`http://localhost:8083/api/todos/byuser/${userId}`)
+        .then(res => res.json())
+        .then(userTodo => {
+            console.log(userTodo);
+            displayUserTodo(userTodo)
+        })
+        .catch(err => {
+            console.error(`NOT FETCHING THE USER TODO`, err);
+        });
+}
+
+
+//
+function displayUserTodo(){
+
 }
